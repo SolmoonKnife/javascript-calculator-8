b888b0f04bcef70d01385f85628e3af0727919c2
Pu-reun-deul Kim <prd106401@naver.com>
2025-10-20 21:16:10 +0900

    feat: 계산기 모듈을 구현했습니다.
    계산기는 기본 구분자와 커스텀 구분자를 따로 처리할 수 있습니다.\n구분자를 제외한 숫자들의 합을 계산하여 반환합니다.\n\n1. 기본 구분자(,|:)가 혼합되어도 파싱할 수 있습니다.\n2. '//', '\n' 사이의 문자 하나를 커스텀 구분자로 사용할 수 있습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from "@woowacourse/mission-utils"
+import { calculator } from "./Calculator.js";
+
 class App {
-  async run() {}
+  async run() {
+    //Input & Print Test
+    const inputString = await Console.readLineAsync("덧셈할 문자열을 입력해주세요.");
+    Console.print(`입력 문자열: ${inputString}`);
+
+    //Calculator Test
+  }
 }
 
 export default App;
---
+++ src/Calculator.js
@@ -0,0 +1,22 @@
+export function calculator(inputString) {
+    if (!inputString || typeof inputString !== 'string') {
+        throw new Error("[ERROR] 문자열이 잘못 입력되었습니다.");
+    }
+
+    const customSlicerRegex = /^\/\/(.)\n/; // '//'로 시작해야 하며, '// /n' 사이의 문자 1개 캡쳐
+    const normalSlicerRegex = /,|:/; // 기본 구분자 , 혹은 : 를 찾습니다
+
+    const match = inputString.match(customSlicerRegex);
+
+    let numbers = [];
+    if (match) {
+        const customSlicer = match[1]; // ex) ';'
+        const numbersPart = inputString.substring(match[0].length); // ex) '//;\n' 이후부터 취한다
+
+        numbers = numbersPart.split(customSlicer).map(Number);
+    } else {
+        numbers = inputString.split(normalSlicerRegex).map(Number);
+    }
+
+    return numbers.reduce((acc, number) => acc + number, 0)
+}
