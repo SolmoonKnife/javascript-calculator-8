a298bdefc959f201c44eb7fbdf7a3b6125e1b1d5
Pu-reun-deul Kim <prd106401@naver.com>
2025-10-20 22:25:26 +0900

    feat: index.js에서 run() 실행 중 비동기 에러를 처리하는 로직을 추가했습니다.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import App from "./App.js";
 
 const app = new App();
-await app.run();
+// run() 함수 실행 과정에서 일어날 수 있는 각종 비동기 에러를 처리합니다.
+await app.run().catch((e) => {
+    console.log(e.message);
+});
